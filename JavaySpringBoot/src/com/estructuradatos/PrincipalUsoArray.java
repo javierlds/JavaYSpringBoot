79f6d611a1e04527bba89528b712e17fd66ae387
javierlds <javierlds@gmail.com>
2024-03-19 21:36:57 -0500

    Avance uso arreglos creacion programa ejemplo

--- JavaySpringBoot/src/com/estructuradatos/PrincipalUsoArray.java
+++ JavaySpringBoot/src/com/estructuradatos/PrincipalUsoArray.java
@@ -1,8 +1,48 @@
 package com.estructuradatos;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class PrincipalUsoArray {
     public static void main(String args[]){
-        UsoArrayList usoArr = new UsoArrayList();
+        boolean entradaValida = false;
+        int tamanoArreglo;
+        int numerosUsuario[];
+        int numero;
+        int opcion;
+        UsoArrayList usor = new UsoArrayList();
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Ingrese tamaño del arreglo");
+        tamanoArreglo = scan.nextInt();
+        numerosUsuario = new int[tamanoArreglo];
+
+        while(!entradaValida){
+            try{
+
+                System.out.println("Llene el arreglo....");
+                for(int i = 1; i <= tamanoArreglo; i++){
+                    System.out.println("Ingrese numero:" + i);
+                    numero = scan.nextInt();
+                    numerosUsuario[i] = numero;
+                }
+
+                entradaValida = true;
+            }catch(InputMismatchException e){
+                System.out.println("Valor invalido, por favor ingrese un numero");
+                scan.next();
+            }
+        }
+        System.out.println("tamaño del arreglo:" + numerosUsuario.length);
+        System.out.println("Seleccione una de las siguientes opciones");
+        System.out.println("1. Ordenar arreglo");
+        System.out.println("2. Numero mayor del arreglo");
+        System.out.println("3. Numero menor del arreglo");
+        System.out.println("4. Numeros pares arreglo");
+        System.out.println("5. Numeros impares arreglo");
+        opcion = scan.nextInt();
+        if(opcion == 1){
+            usor.ordenarArreglo(numerosUsuario);
+        }
     }
 }
--- JavaySpringBoot/src/com/estructuradatos/UsoArrayList.java
+++ JavaySpringBoot/src/com/estructuradatos/UsoArrayList.java
@@ -1,8 +1,11 @@
 package com.estructuradatos;
 
+import java.util.Arrays;
+
 public class UsoArrayList {
     private String respuesta = "";
     private int numeros[];
+    private int totalElementos;
     private Persona persona[]; //arreglos de personas
     public void arregloEnteros(){
         numeros = new int[3];//de tamaño 3 -- Valor por defecto para enteros es 0
@@ -11,6 +14,16 @@ public class UsoArrayList {
         numeros[2] = 30;
 
     }
+
+    public void ordenarArreglo(int numeroUsu[]){
+        totalElementos =  numeroUsu.length;
+        numeroUsu = new int[totalElementos];
+        Arrays.sort(numeroUsu);
+        for(int numero : numeroUsu){
+            System.out.println("Arreglo ordenado:" + numero);
+        }
+    }
+
     public String adicionaCliente(){
 
         return respuesta;
